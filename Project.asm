b90b877c9d5c372c408b4cd8031413c010f1fcc8
Bhavik Patel <Bhavik Patel@WIN-FJSVKLLO8LC>
2025-10-15 16:31:30 -0700

    Creates all of the variables and user messages.

---
+++ Project.asm
@@ -0,0 +1,59 @@
+.386
+.model flat, stdcall
+.stack 4096
+
+INCLUDE Irvine32.inc
+
+ExitProcess PROTO, dwExitCode:DWORD
+WriteString PROTO
+WriteChar PROTO
+ReadInt PROTO
+Crlf PROTO
+Randomize PROTO
+RandomRange PROTO
+SetTextColor PROTO
+GetTextColor PROTO
+Delay PROTO
+
+.data
+MAX_ATTEMPTS = 100 ;maximum number of user attempts
+MIN_NUM = 1 ;minimum value of a number
+MAX_NUM = 5000 ;maximum value of a number
+num1 DWORD ? ;stores the value of the first number
+num2 DWORD ? ;stores the value of the second number
+guess1 DWORD ? ;stores the users most recent guess for the first number
+guess2 DWORD ? ;stores the users most recent guess for the second number
+attempts DWORD 0 ;stores the number of attempts the user has used so far
+gameWon BYTE 0 ;stores the status of the current game (if the user has won or not)
+
+; Messages
+welcomeMSG BYTE "Welcome to the Animated Number Guessing Game!", 0
+instructions BYTE "I'm thinking of two numbers between 1 and 5000. Try and guess them!", 0
+prompt1 BYTE "Enter your guess for the first number: ", 0
+prompt2 BYTE "Enter your guess for the second number: ", 0
+correctMSG BYTE "Congrats! You guessed correctly!", 0
+tooHighMSG BYTE "Too high!", 0
+tooLowMSG BYTE "Too low!", 0
+attemptsMSG BYTE "Attempts: ", 0
+gameOverMSG BYTE "Game Over! the numbers were: ", 0
+playAgainMSG BYTE "Play again? (1=Yes, 0=No): ", 0
+
+;Color Constants
+BLUE = 1
+GREEN = 2
+RED = 4
+INTENSE =8
+WHITE = 15
+
+
+
+
+
+
+
+
+
+
+
+
+
